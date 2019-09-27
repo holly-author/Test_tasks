3a78265063abf40ab9135e39e0aeea416267fa7f
Alex Mir <marginalauthor@gmail.com>
2019-09-27 23:30:17 +0300

    Update and rename Light IT 2.py to LightIT.py

--- LightIT.py
+++ LightIT.py
@@ -1,49 +1,28 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 import random
 import keyboard
 
 class Play():
     def low_damage():
         damage = random.randint(18,26)
-        return damage
-    
+        return damage    
     def heavy_damage():
         damage = random.randint(10,36)
-        return damage
-    
+        return damage   
     def heal():
         heal=random.randint(18,26)
         return heal
 
-
-# In[ ]:
-
-
 class Player(Play):
     health=100
     name = 'Player 1'
-
 class Computer(Play):
     health=100
     name = 'Player 2'
-
-
-# In[ ]:
-
 
 # Вывод уровеня здоровья каждого игрока
 def health_state():
     print('{} health: {}'.format(player_1.name, player_1.health))
     print('{} health: {}'.format(player_2.name, player_2.health))
-
-
-# In[ ]:
-
 
 # Функция проверяет завершилась ли игра, если уровень здоровья одного из игроков упал ниже нуля.
 def game_over():
@@ -56,51 +35,33 @@ def game_over():
     else:
         return True
 
-
-# In[ ]:
-
-
 # Действие вибирается случаныйным образом из 3-х возможных, для каждого присваивается свой номер.
-# Если уровень здоровья компьютера ниже 35, выполняется выбор из 2-х вариантов.
-    
+# Если уровень здоровья компьютера ниже 35, выполняется выбор из 2-х вариантов.  
 def action():
     if player_2.health < 35:
         action = random.choice([1,3])
     else:
-        action = random.choice([1,2,3])
-        
+        action = random.choice([1,2,3])       
     # Для каждого действия вызывается соответствующая функция из класса Play        
     if action == 1:
         damage_level = next_player.low_damage()
         next_player.health -= damage_level
-        print('{} used low damage -{}'.format(curr_player.name,damage_level))
-        
+        print('{} used low damage -{}'.format(curr_player.name,damage_level))       
     elif action == 2:
         damage_level = next_player.heavy_damage()
         next_player.health-= damage_level
-        print('{} used heavy damage -{}'.format(curr_player.name,damage_level))
-        
+        print('{} used heavy damage -{}'.format(curr_player.name,damage_level))    
     else:
         heal_level = curr_player.heal()
         curr_player.health+=heal_level
-        print('{} healed himself +{}'.format(curr_player.name,heal_level))
-        
+        print('{} healed himself +{}'.format(curr_player.name,heal_level))    
     health_state()
-
-
-# In[ ]:
-
 
 player_1 = Player
 player_2 = Computer
 
-
-# In[ ]:
-
-
 # Выбор первого игрока происходит рандомно, 1 - для игрока, 2 - для компьютера.
 # Curr_player хранит текущего игрока, а next_player - чей ход будет следующим.
-
 curr_player = random.choice([1,2])
 if curr_player == 1:
     curr_player = player_1
@@ -129,4 +90,3 @@ while game:
         keyboard.wait('space')
     print('--------------------------------------------------------')
     game = game_over()
-
